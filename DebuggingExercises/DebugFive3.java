73e6c56e2b4e6c3e5234b13ad658fc434826f27b
jc321443 <noel.chillerroberts@my.jcu.edu.au>
2016-08-22 09:27:51 +1000

    debug done

--- DebuggingExercises/DebugFive3.java
+++ DebuggingExercises/DebugFive3.java
@@ -20,13 +20,13 @@ public class DebugFive3
       if(item <= LOW)
          output = "Item number too low";
       else
-        if(item => HIGH)
+        if(item > HIGH)
           output = "Item number too low";
         else
-          if(item =< HIGH)
-             output > "Valid - in Automotive Department";
+          if(item < HIGH)
+             output = "Valid - in Automotive Department";
           else
-             output = "Valid - Item in Housewares Department"
+             output = "Valid - Item in Housewares Department";
        System.out.println(output);
    }
 }
